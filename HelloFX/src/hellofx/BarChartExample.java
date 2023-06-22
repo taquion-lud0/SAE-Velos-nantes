115e1668986536a9cbbf428d9be28da00a27c2b9
Mathieu-Stephan <Matsteph56@gmail.com>
2023-06-22 12:12:51 +0200

    Plus besoin de "vers"

--- HelloFX/src/hellofx/BarChartExample.java
+++ HelloFX/src/hellofx/BarChartExample.java
@@ -78,4 +78,121 @@ public class BarChartExample {
         return chart;
         
     }
+
+    public static JFreeChart affluenceSansHGraph(ResultSet set){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        System.out.println("affluenceGraph");
+
+        try{
+            while (set.next()) {
+                String nomCompteur = set.getString("nomCompteur");
+                String sens = set.getString("sens");
+                //concaténation du nom du compteur et du sens pour avoir un nom unique
+                nomCompteur = nomCompteur + " " + sens;
+                String jour = set.getString("dateComptage");
+                int nbVelos = set.getInt("nbVelos");
+                dataset.addValue(nbVelos, nomCompteur, jour);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        // Création du graphique
+        JFreeChart chart = ChartFactory.createBarChart(
+            "Affluence", // Titre du graphique 
+            "Compteurs", // Axe des x
+            "Nombre de vélos", // Axe des y
+            dataset, // Jeu de données
+            PlotOrientation.VERTICAL, // Orientation du graphique
+            true, // Afficher la légende
+            true, // Générer les tooltips
+            false // Générer les URLs
+        );
+
+        return chart;
+    }
+
+    public static JFreeChart affluenceAvecHGraph(ResultSet set){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        System.out.println("affluenceGraph");
+
+        try{
+            while (set.next()) {
+                String nomCompteur = set.getString("nomCompteur");
+                String sens = set.getString("sens");
+                //concaténation du nom du compteur et du sens pour avoir un nom unique
+                nomCompteur = nomCompteur + " " + sens;
+                String h00 = set.getString("h00");
+                String h01 = set.getString("h01");
+                String h02 = set.getString("h02");
+                String h03 = set.getString("h03");
+                String h04 = set.getString("h04");
+                String h05 = set.getString("h05");
+                String h06 = set.getString("h06");
+                String h07 = set.getString("h07");
+                String h08 = set.getString("h08");
+                String h09 = set.getString("h09");
+                String h10 = set.getString("h10");
+                String h11 = set.getString("h11");
+                String h12 = set.getString("h12");
+                String h13 = set.getString("h13");
+                String h14 = set.getString("h14");
+                String h15 = set.getString("h15");
+                String h16 = set.getString("h16");
+                String h17 = set.getString("h17");
+                String h18 = set.getString("h18");
+                String h19 = set.getString("h19");
+                String h20 = set.getString("h20");
+                String h21 = set.getString("h21");
+                String h22 = set.getString("h22");
+                String h23 = set.getString("h23");
+                dataset.addValue(Integer.parseInt(h00), nomCompteur, "00h");
+                dataset.addValue(Integer.parseInt(h01), nomCompteur, "01h");
+                dataset.addValue(Integer.parseInt(h02), nomCompteur, "02h");
+                dataset.addValue(Integer.parseInt(h03), nomCompteur, "03h");
+                dataset.addValue(Integer.parseInt(h04), nomCompteur, "04h");
+                dataset.addValue(Integer.parseInt(h05), nomCompteur, "05h");
+                dataset.addValue(Integer.parseInt(h06), nomCompteur, "06h");
+                dataset.addValue(Integer.parseInt(h07), nomCompteur, "07h");
+                dataset.addValue(Integer.parseInt(h08), nomCompteur, "08h");
+                dataset.addValue(Integer.parseInt(h09), nomCompteur, "09h");
+                dataset.addValue(Integer.parseInt(h10), nomCompteur, "10h");
+                dataset.addValue(Integer.parseInt(h11), nomCompteur, "11h");
+                dataset.addValue(Integer.parseInt(h12), nomCompteur, "12h");
+                dataset.addValue(Integer.parseInt(h13), nomCompteur, "13h");
+                dataset.addValue(Integer.parseInt(h14), nomCompteur, "14h");
+                dataset.addValue(Integer.parseInt(h15), nomCompteur, "15h");
+                dataset.addValue(Integer.parseInt(h16), nomCompteur, "16h");
+                dataset.addValue(Integer.parseInt(h17), nomCompteur, "17h");
+                dataset.addValue(Integer.parseInt(h18), nomCompteur, "18h");
+                dataset.addValue(Integer.parseInt(h19), nomCompteur, "19h");
+                dataset.addValue(Integer.parseInt(h20), nomCompteur, "20h");
+                dataset.addValue(Integer.parseInt(h21), nomCompteur, "21h");
+                dataset.addValue(Integer.parseInt(h22), nomCompteur, "22h");
+                dataset.addValue(Integer.parseInt(h23), nomCompteur, "23h");
+                
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        // Création du graphique
+        
+        JFreeChart chart = ChartFactory.createBarChart(
+            "Affluence", // Titre du graphique 
+            "Compteurs", // Axe des x
+            "Nombre de vélos", // Axe des y
+            dataset, // Jeu de données
+            PlotOrientation.VERTICAL, // Orientation du graphique
+            true, // Afficher la légende
+            true, // Générer les tooltips
+            false // Générer les URLs
+        );
+
+        return chart;
+
+
+    }
 }
--- HelloFX/src/hellofx/Requetes.java
+++ HelloFX/src/hellofx/Requetes.java
@@ -6,9 +6,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import org.jfree.chart.JFreeChart;
+
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import org.jfree.chart.ChartFrame;
 
 
 public class Requetes {
@@ -20,15 +23,23 @@ public class Requetes {
 
     public static void main(String[] args) {
         //Exemple de requete
-        String pisteD = "Vn751A vers St Leger Les Vignes";
-        String pisteA = "Vn vers Suce Sur Erdre";
+        String pisteD = "Vn751A St Leger les Vignes";
+        String pisteA = "VN Suce sur Erdre";
         String date = "2022-01-01";
         String hD = "h16";
         String hA = "h17";
         int affluence = 0;
 
-        ResultSet res = trafficJournalier(date, hD);
-        BarChartExample bce = new BarChartExample();
+        /*
+        ResultSet res = affluenceAvecH(date, date, pisteA, 0);
+        JFreeChart chart = BarChartExample.affluenceAvecHGraph(res);
+        //affiche le graphique
+        ChartFrame frame = new ChartFrame("Affluence", chart);
+        frame.pack();
+        frame.setVisible(true);*/
+        String test = itineraire(pisteD, pisteA);
+        System.out.println(test);
+        
         //bce.trafficJournalierGraph(res);
         //bce.pisteEnvironGraph(res);
 
@@ -88,12 +99,11 @@ public class Requetes {
 
     public static ResultSet pisteEnviron(String piste){
         ResultSet res = null;
-        String[] SplitnomPiste = piste.split(" ");
-        String sens = SplitnomPiste[SplitnomPiste.length-1];
-        String nomPiste = "";
-        for(int i = 0; i < SplitnomPiste.length-1; i++){
-            nomPiste = nomPiste + SplitnomPiste[i] + " ";
-        }
+        String[] pistSplit = clean(piste);
+        String nomPiste = pistSplit[0];
+        String sens = pistSplit[1];
+        System.out.println(nomPiste);
+
 
         try {
             String url = "jdbc:mysql://localhost:3306/velo_bdd";
@@ -114,18 +124,17 @@ public class Requetes {
 
     public static String itineraire(String pisteD, String pisteA){
         String itineraire = "";
-        //separe le nom de la piste et le sens tous ce qui est avant "vers" est le nom de la piste et ce qui est apres est le sens
-        String nomPisteD = pisteD.split(" vers ")[0];
-        String sensD = pisteD.split(" vers ")[1];
-        String nomPisteA = pisteA.split(" vers ")[0];
-        String sensA = pisteA.split(" vers ")[1];
-        nomPisteD = nomPisteD + " ";
-        nomPisteA = nomPisteA + " ";
+        String[] pistSplitD = clean(pisteD);
+        String[] pistSplitA = clean(pisteA);
+        String nomPisteD = pistSplitD[0];
+        String sensD = pistSplitD[1];
+        String nomPisteA = pistSplitA[0];
+        String sensA = pistSplitA[1];
 
         try{
             String url = "jdbc:mysql://localhost:3306/velo_bdd";
             String user = "root";
-            String password = "azerty123";
+            String password = "123456";
 
             Connection connection = DriverManager.getConnection(url, user, password);
             String query = "SELECT CONCAT(c2.nomCompteur, c2.sens) AS compteur_Intermédiaire, ACOS(SIN(RADIANS(c1.COORD_X)) * SIN(RADIANS(c2.COORD_X)) + COS(RADIANS(c1.COORD_X)) * COS(RADIANS(c2.COORD_X)) * COS(RADIANS(c2.COORD_Y - c1.COORD_Y))) * 6371  AS dist_Départ_Chemin FROM Compteur c1, Compteur c2, Compteur c3  WHERE c1.nomCompteur != c2.nomCompteur AND c2.nomCompteur  != c3.nomCompteur AND c1.nomCompteur != c3.nomCompteur AND UPPER(c1.nomCompteur) = '"+nomPisteD+"' AND UPPER(c1.sens) = '"+ sensD +"' AND UPPER(c3.nomCompteur) = '"+nomPisteA+"'  AND UPPER(c3.sens) = '"+sensA+"' AND c1.COORD_X <= c2.COORD_X  AND c2.COORD_X <= c3.COORD_X AND c1.COORD_Y <= c2.COORD_Y AND c2.COORD_Y <= c3.COORD_Y ORDER BY dist_Départ_Chemin;";
@@ -151,12 +160,12 @@ public class Requetes {
 
     }
 
-    public static ResultSet affluence(String dateD, String dateF, String hD, String hA, String piste, int affluence){
+    public static ResultSet affluenceSansH(String dateD, String dateF, String piste, int affluence){
         ResultSet res = null;
-        //separe le nom de la piste et le sens tous ce qui est avant "vers" est le nom de la piste et ce qui est apres est le sens
-        String nomPiste = piste.split(" vers ")[0];
-        String sens = piste.split(" vers ")[1];
-        nomPiste = nomPiste + " ";
+        String[] pistSplit = clean(piste);
+        String nomPiste = pistSplit[0];
+        String sens = pistSplit[1];
+
 
         //nb de jours entre les 2 dates
         int nbJours = (int) ChronoUnit.DAYS.between(LocalDate.parse(dateD), LocalDate.parse(dateF));
@@ -169,13 +178,96 @@ public class Requetes {
         String password = "123456";
 
         if(affluence == 0){
-            if(hD.equals("h00") && hA.equals("h23")){
-
+            try {
+                Connection connection = DriverManager.getConnection(url, user, password);
+                String query = "SELECT nomCompteur, sens, dateComptage, SUM(h00 + h01 + h02 + h03 + h04 + h05 + h06 + h07 + h08 + h09 + h10 + h11 + h12 + h13 + h14 + h15 + h16 + h17 + h18 + h19 + h20 + h21 + h22 + h23) AS nbVelos FROM Comptage, Compteur WHERE Comptage.leCompteur = Compteur.idCompteur AND dateComptage BETWEEN '"+dateD+"' AND '"+dateF+"' AND nomCompteur = '"+nomPiste+"' GROUP BY nomCompteur, sens, dateComptage ORDER BY nbVelos DESC  LIMIT " + nbJours +";";
+                System.out.println(query);
+                Statement statement = connection.createStatement();
+                res = statement.executeQuery(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            try{
+                Connection connection = DriverManager.getConnection(url, user, password);
+                String query = "SELECT nomCompteur, sens, dateComptage, SUM(h00 + h01 + h02 + h03 + h04 + h05 + h06 + h07 + h08 + h09 + h10 + h11 + h12 + h13 + h14 + h15 + h16 + h17 + h18 + h19 + h20 + h21 + h22 + h23) AS nbVelos FROM Comptage, Compteur WHERE Comptage.leCompteur = Compteur.idCompteur AND dateComptage BETWEEN '"+dateD+"' AND '"+ dateF +"' GROUP BY nomCompteur, sens, h00, h01, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13, h14, h15, h16, h17, h18, h19, h20, h21, h22, h23 ORDER BY nbVelos "+affluenceRes+" LIMIT 1;";
+                Statement statement = connection.createStatement();
+                res = statement.executeQuery(query);
+            }
+            catch(SQLException e){
+                e.printStackTrace();
             }
 
         }
-        else{
+        return res;
+    }
 
+    public static ResultSet affluenceAvecH(String dateD, String dateF, String piste, int affluence){
+        ResultSet res = null;
+        String[] pistSplit = clean(piste);
+        String nomPiste = pistSplit[0];
+        System.out.println(nomPiste);
+        String sens = pistSplit[1];
+
+
+        //nb de jours entre les 2 dates
+        int nbJours = (int) ChronoUnit.DAYS.between(LocalDate.parse(dateD), LocalDate.parse(dateF));
+        String affluenceRes = "ASC";
+        if(affluence == 1){
+            affluenceRes = "DESC";
+        }
+        String url = "jdbc:mysql://localhost:3306/velo_bdd";
+        String user = "root";
+        String password = "123456";
+        if(affluence == 0){
+            try {
+                Connection connection = DriverManager.getConnection(url, user, password);
+                String query = "SELECT nomCompteur, sens, h00 , h01 , h02 , h03 , h04 , h05 , h06 , h07 , h08 , h09 , h10 , h11 , h12 , h13 , h14 , h15 , h16 , h17 , h18 , h19  ,h20 , h21 , h22 , h23 FROM Comptage, Compteur WHERE Comptage.leCompteur = Compteur.idCompteur AND dateComptage BETWEEN '"+dateD+"' AND '"+dateF+"' AND nomCompteur = '"+nomPiste+"' GROUP BY nomCompteur, sens, h00 , h01 , h02 , h03 , h04 , h05 , h06 , h07 , h08 , h09 , h10 , h11 , h12 , h13 , h14 , h15 , h16 , h17 , h18 , h19  ,h20 , h21 , h22 , h23  LIMIT 1;";
+                System.out.println(query);
+                Statement statement = connection.createStatement();
+                res = statement.executeQuery(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            try{
+                    Connection connection = DriverManager.getConnection(url, user, password);
+                    String query = "SELECT nomCompteur, sens,  h00, h01, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13, h14, h15, h16, h17, h18, h19, h20, h21, h22, h23 FROM Comptage, Compteur WHERE Comptage.leCompteur = Compteur.idCompteur AND dateComptage BETWEEN '"+dateD+"' AND '"+ dateF +"' GROUP BY nomCompteur, sens, h00, h01, h02, h03, h04, h05, h06, h07, h08, h09, h10, h11, h12, h13, h14, h15, h16, h17, h18, h19, h20, h21, h22, h23  LIMIT 1;";
+                    Statement statement = connection.createStatement();
+                    res = statement.executeQuery(query);
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                }
+        }
+
+        return res;
+    }
+
+    public static String[] clean(String r){
+        String[] res = new String[2];
+        //verifie si la chaine se termine par "Gare Sud"
+        if(r.endsWith("Gare Sud")){
+            res[0] = r.substring(0, r.length()-9) + " ";
+            res[1] = "Gare Sud";
+        }
+        //verifie si la chaine se termine par "St Leger les Vignes"
+        else if(r.endsWith("St Leger les Vignes")){
+            res[0] = r.substring(0, r.length()-20) + " ";
+            res[1] = "St Leger les Vignes";
+        }
+        //verifie si la chaine se termine par "Suce sur Erdre"
+        else if(r.endsWith("Suce sur Erdre")){
+            res[0] = r.substring(0, r.length()-14);
+            res[1] = "Suce sur Erdre";
+        }
+        else{
+            //res[1] est le dernier mot de la chaine
+            res[1] = r.substring(r.lastIndexOf(" ")+1);
+            //res[0] est la chaine sans le dernier mot
+            res[0] = r.substring(0, r.lastIndexOf(" ")) + " ";
         }
         return res;
     }
